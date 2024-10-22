23f82ac5c7251d25e35acd696ce031fc606eade7
syrbox <navarreteagustin93@gmail.com>
2024-10-22 04:10:43 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/characters',
+    pathMatch: 'full',
+  },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./main-container/main-container.component').then(
+        (m) => m.MainContainerComponent
+      ),
+  },
+  {
+    path: 'characters/:id',
+    loadComponent: () =>
+      import(
+        './main-container/components/character-add-edit/character-add-edit.component'
+      ).then((m) => m.CharacterAddEditComponent),
+  },
+];
